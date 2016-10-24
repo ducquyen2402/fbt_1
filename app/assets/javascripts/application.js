c0e79918d3f8799b10cdb02153bec3c9fadaef84
ninh.duc.quyen <ninh.duc.quyen@gmail.com>
2016-10-24 21:21:32 +0700

    like plugin

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -67,3 +67,19 @@ $(document).ready(function() {
   myTimer = setTimeout("showNext()", 3000);
   showNext();
 });
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.8";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.8";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
---
+++ redis-stable/src/release.h
@@ -0,0 +1,3 @@
+#define REDIS_GIT_SHA1 "2092955b"
+#define REDIS_GIT_DIRTY "32"
+#define REDIS_BUILD_ID "quyen-K55A-1477306215"
