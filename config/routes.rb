ba8cec227c95c9e2c778f1b4837456087368c5cc
ninh.duc.quyen <ninh.duc.quyen@gmail.com>
2016-10-27 13:41:58 +0700

    rating

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  post '/rate' => 'rater#create', :as => 'rate'
   mount Ckeditor::Engine => "/ckeditor"
   resources :reviews
   resources :categories, only: :show
